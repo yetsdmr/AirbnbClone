07a4e964b29870dcab13cfc39171f60eb7023bce
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-10-19 23:29:13 +0300

    Initial Commit

---
+++ AirbnbClone/AirbnbCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbCloneApp.swift
+//  AirbnbClone
+//
+//  Created by Yunus Emre Taşdemir on 19.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
