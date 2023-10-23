155464e241d4b718f9fbb10246daf1018778d78a
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-10-23 16:24:53 +0300

    Added Explore View UI.

---
+++ AirbnbClone/Core/Listings/ListingItemView.swift
@@ -0,0 +1,70 @@
+//
+//  ListingView.swift
+//  AirbnbClone
+//
+//  Created by Yunus Emre Taşdemir on 23.10.2023.
+//
+
+import SwiftUI
+
+struct ListingItemView: View {
+    
+    var images = [
+        "listing-1",
+        "listing-2",
+        "listing-3",
+        "listing-4",
+    ]
+    
+    var body: some View {
+        VStack(spacing: 8) {
+            // image
+            TabView {
+                ForEach(images, id:\.self) { image in
+                    Image(image)
+                        .resizable()
+                        .scaledToFill()
+                }
+            }
+            .frame(height: 320)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .tabViewStyle(.page)
+            
+            
+            // listing detail
+            HStack(alignment: .top) {
+                // details
+                VStack(alignment: .leading) {
+                    Text("Miami, Florida")
+                        .fontWeight(.semibold)
+                    
+                    Text("12 mi away")
+                        .foregroundStyle(.gray)
+                    
+                    Text("Nov 3 - 10")
+                        .foregroundStyle(.gray)
+                    
+                    HStack(spacing: 4) {
+                        Text("$567")
+                            .fontWeight(.semibold)
+                        Text("night")
+                    }
+                }
+                
+                Spacer()
+                
+                // rating
+                HStack(spacing: 2) {
+                    Image(systemName: "star.fill")
+                    
+                    Text("4.86")
+                }
+            }
+            .font(.footnote)
+        }
+    }
+}
+
+#Preview {
+    ListingItemView()
+}
